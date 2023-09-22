a02d323c332e9a8fb20d6f07bcfdbe6c5b6e6fd5
marlosb <marlosbosso@outlook.com>
2023-09-22 16:25:26 -0300

    change main.py to inform session numebr and range distance from command line

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ if __name__ == "__main__":
     import sys
 
     if 'event' in sys.argv:
-        next_event = F1Event('Hungary')
+        next_event = F1Event('Suzuka')
         response = model.predict_messages(event_prompt.format_messages(event_info = next_event.event))
         print(response.content)
     elif 'session' in sys.argv:
-        session = F1Session('Hungary', 2, drivers)
+        session_number = sys.argv[sys.argv.index('session') + 1]
+        session = F1Session('Suzuka', session_number, drivers)
         briefer = SessionBriefer(session)
         kwargs = briefer.create_session_brief()
         response = model.predict_messages(session_prompt.format_messages(**kwargs))
@@ -24,18 +25,23 @@ if __name__ == "__main__":
         ploter = Ploter(session)
         ploter.plot_comparison()
     elif 'race' in sys.argv:
-        session = F1Session('Silverstone', 5, drivers)
+        session = F1Session('Suzuka', 5, drivers)
         briefer = SessionBriefer(session)
         kwargs = briefer.create_race_brief()
         response = model.predict_messages(race_prompt.format_messages(**kwargs))
         print(response.content)
     elif 'range' in sys.argv:
-        session = F1Session('Silverstone', 4, drivers)
+        index = sys.argv.index('range')
+        session_number = sys.argv[index + 1]
+        range_start = int(sys.argv[index + 2])
+        range_end = int(sys.argv[index + 3])
+        lap_section = [range_start, range_end]
+        session = F1Session('Suzuka', session_number, drivers)
         briefer = SessionBriefer(session)
-        template = briefer.create_range_briefing([5300,5530], turn_name = '15')
+        template = briefer.create_range_briefing(lap_section, turn_name = 'chincane')
         response = model.predict_messages(range_prompt.format_messages(range_info = template))
         print(response.content)
         ploter = Ploter(session)
-        ploter.plot_comparison(chart_range=[5300,5530],
-                               features= ['Time_Gap', 'Speed', 'Brake', 'Throttle'])  
+        ploter.plot_comparison(chart_range=lap_section,
+                               features= ['Speed', 'Brake', 'Throttle'])  
                                
